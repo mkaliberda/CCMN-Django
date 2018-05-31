68c9a7717ad80b65d06f837b465a5a628dc322a4
Maksym Kaliberda <mkaliber@e1r3p15.unit.ua>
2018-05-31 18:30:29 +0300

    add api token

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
 # Register your models here.
+from .models import *
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Profile._meta.fields]
+    search_fields = [field.name for field in Profile._meta.fields]
+    list_filter = [field.name for field in Profile._meta.fields]
+
+admin.site.register(Profile, ProfileAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name="notes",
+                              on_delete=models.CASCADE, null=True)
+    text = models.CharField(max_length=255)
+    register_date = models.DateTimeField(auto_now_add=True)
+
+
