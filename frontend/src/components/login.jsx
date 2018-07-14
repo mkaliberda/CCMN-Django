c8c67e8a9035566675a19b60a8167990c6c5482e
dspyrydo <dspyrydo@student.unit.ua>
2018-07-14 20:56:13 +0300

    WIP

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,13 +1,40 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import 'antd/dist/antd.css'
-import { Card, Input, Button } from 'antd'
+import { Card, Input, Button, Form, Icon } from 'antd'
 import { connect } from 'react-redux'
 import { push } from 'react-router-redux'
 
-const styles = {
+const FormItem = Form.Item
 
-}
+const styles = ({
+    divWrapper: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: '15%',
+    },
+    input: {
+        width: '50%',
+        marginBottom: 0,
+    },
+    form: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    buttons: {
+        marginTop: 10,
+        marginBottom: 10
+    },
+    card: {
+        width: 600,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+})
 
 class Login extends React.Component {
 
@@ -17,22 +44,22 @@ class Login extends React.Component {
 
     render() {
         return (
-          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: '15%'}}>
-            <Card title="CCMN" style={{ width: 600, display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-            <p>Cisco is the worldwide leader in IT and networking. 
-            Cisco helps companies of all sizes and people to connect, communicate, and collaborate. 
-            Many companies have provided public APIs as a means for others to access their infrastructure. 
-            During the realization of this project, you can practice with API and make your own app or program. 
-            You must develop solutions that will use real-time intelligence gathered from the UNIT Factory Wi-Fi 
-            network to enable people and their devices to interact more effectively through real-time contextual 
-            information related to such parameters as locations, availability of users, or mobile device assets.</p>
-            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                <Input style={{ width: '50%' }} placeholder='login'/>
-                <Input style={{ width: '50%' }} placeholder='password'/>
-                <Button style={{ marginTop: 10, marginBottom: 10 }} type="primary">Login</Button> 
-                Or
-                <Button onClick={this.goToSignupPage} style={{ marginTop: 10, marginBottom: 10 }} type="secondary">Get Your Access</Button>
-            </div>
+          <div style={styles.divWrapper}>
+            <Card title="CCMN" style={styles.card}>
+                <p>Cisco is the worldwide leader in IT and networking. 
+                Cisco helps companies of all sizes and people to connect, communicate, and collaborate. 
+                Many companies have provided public APIs as a means for others to access their infrastructure. 
+                During the realization of this project, you can practice with API and make your own app or program. 
+                You must develop solutions that will use real-time intelligence gathered from the UNIT Factory Wi-Fi 
+                network to enable people and their devices to interact more effectively through real-time contextual 
+                information related to such parameters as locations, availability of users, or mobile device assets.</p>
+                <Form style={styles.form}>
+                    <Input style={styles.input} prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />    
+                    <Input style={styles.input} prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                    <Button style={styles.buttons} type="primary">Login</Button>
+                        Or
+                    <Button onClick={this.goToSignupPage} style={styles.buttons} type="secondary">Get Your Access</Button>
+                </Form>
             </Card>
           </div>
         )
--- frontend/src/components/signup.jsx
+++ frontend/src/components/signup.jsx
@@ -1,12 +1,47 @@
 import React from 'react'
 import 'antd/dist/antd.css'
 import input from 'antd/lib/input';
+import { Card, Input, Button, Form, Icon } from 'antd'
 
+const styles = ({
+    divWrapper: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: '15%',
+    },
+    form: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    input: {
+        width: '50%',
+        marginBottom: 0,
+    },
+    card: {
+        width: 600,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+})
 class SignUp extends React.Component {
     render() {
+        //TODO: change icons
         return (
-            <div>
-            SignUp page
+            <div style={styles.divWrapper}>
+                <Card title="CCMN" style={styles.card}>
+                    <p>Get your access</p>
+                    <Form style={styles.form}>
+                    <Input style={styles.input} prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="First Name" />
+                    <Input style={styles.input} prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Last Name" />
+                    <Input style={styles.input} prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Company Website URL" />
+                    <Input style={styles.input} prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Email" />
+                    </Form>
+                    <Button style={styles.buttons} type="primary">Get My Access Link</Button>
+                </Card>
             </div>
         )
     }
