c18a023fc9d1b07d9986430b0b4720967b4505af
dspyrydo <dspyrydo@student.unit.ua>
2018-08-05 16:25:18 +0300

    delete unnecessarry stuff

--- frontend/src/containers/app/index.js
+++ frontend/src/containers/app/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Route, Link } from 'react-router-dom'
-import Home from '../home'
 import Login from '../../components/login'
 import About from '../about'
 import SignUp from '../../components/signup'
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux'
-import counter from './counter'
 import auth, { authInitialState } from './auth'
 
 const appReducer = combineReducers({auth})
