49727ec0ce44cc00c76d5708c2a2c963583d5a47
dspyrydo <dspyrydo@student.unit.ua>
2018-08-25 21:16:43 +0300

    getting the total number of devices connected

--- frontend/src/components/nav/navToolBar.jsx
+++ frontend/src/components/nav/navToolBar.jsx
@@ -3,16 +3,27 @@ import PropTypes from 'prop-types'
 import {
   Layout, Menu, Breadcrumb, Icon
 } from 'antd'
+import { connect } from 'react-redux'
+import { dispatch } from 'redux-act'
+import moment from 'moment'
+import { getNumberOfOnlineUsers } from '../../reducers/cisco'
 
 const { Header, Content, Sider } = Layout
-
+const styles = {
+  headerItem: {
+    color: 'white',
+  }
+}
 class NavToolBar extends React.Component {
   state = {
-    collapsed: false
+    collapsed: false,
+    dateAndTime: null,
   }
 
   static propTypes = {
     children: PropTypes.any,
+    getNumberOfOnlineUsers: PropTypes.func.isRequired,
+    onlineUsers: PropTypes.number.isRequired,
   }
 
   toggle = () => {
@@ -21,46 +32,82 @@ class NavToolBar extends React.Component {
     }))
   }
 
+  setDateAndTime = () => {
+    this.setState({
+      dateAndTime: moment().format('MMMM Do YYYY, h:mm:ss a')
+    })
+  }
+
+  getTotalDevicesConnected = () => {
+    this.props.getNumberOfOnlineUsers()
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.setDateAndTime, 1000)
+    this.devicesConnectedTimeout = setInterval(this.getTotalDevicesConnected, 15000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+    clearInterval(this.devicesConnectedTimeout)
+  }
+
   render() {
-    const { children } = this.props
+    const { children, onlineUsers } = this.props
+    const { dateAndTime } = this.state
 
     return (
-      <Layout>
-        <Sider
-          trigger={null}
-          collapsible
-          collapsed={this.state.collapsed}
-        >
+      <Layout style={{ height: '100%' }}>
+        <Header className="header">
           <div className="logo" />
-          <Menu
-            mode="inline"
-            theme="dark"
-            defaultSelectedKeys={['1']}
+
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <div style={styles.headerItem}><b>CCMN</b></div>
+            <div style={styles.headerItem}>{dateAndTime}</div>
+            <div style={styles.headerItem}>Total Devices Connected: {onlineUsers}</div>
+            <div style={styles.headerItem}>Connected today:</div>
+            <div style={styles.headerItem}>New connections:</div>
+          </div>
+
+        </Header>
+        <Layout style={{ height: '100%' }}>
+          <Sider
+            trigger={null}
+            collapsible
+            collapsed={this.state.collapsed}
           >
-            <Menu.Item key="1">option1</Menu.Item>
-            <Menu.Item key="2">option2</Menu.Item>
-            <Menu.Item key="3">option3</Menu.Item>
-            <Menu.Item key="4">option4</Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout>
-          <Header style={{ background: '#fff', padding: 0 }}>
-            <Icon
-              className="trigger"
-              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
-              onClick={this.toggle}
-            />
-          </Header>
-          <Content style={{
-            background: '#fff', padding: 24, margin: 0, minHeight: 280
-          }}
-          >
-            {children}
-          </Content>
+            <div className="logo" />
+            <Menu
+              mode="inline"
+              theme="dark"
+              defaultSelectedKeys={['1']}
+            >
+              <Menu.Item key="1">option1</Menu.Item>
+              <Menu.Item key="2">option2</Menu.Item>
+              <Menu.Item key="3">option3</Menu.Item>
+              <Menu.Item key="4">option4</Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout>
+            <Content style={{
+              background: '#fff', padding: 24, margin: 0, minHeight: 280
+            }}
+            >
+              {children}
+            </Content>
+          </Layout>
         </Layout>
       </Layout>
     )
   }
 }
 
-export default NavToolBar
+const mapStateToProps = state => ({
+  onlineUsers: state.cisco.onlineUsers
+})
+
+const mapDispatchToProps = dispatch => ({
+  getNumberOfOnlineUsers: () => dispatch(getNumberOfOnlineUsers())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavToolBar)
